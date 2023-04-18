52a4e09939532c353e947a5d4152ab39f7e27de7
Christian Ani <iamchristianani@gmail.com>
2023-04-18 11:41:22 +0100

    Fix Linter and Rubocop errors

--- test/controllers/recipe_foods_controller_test.rb
+++ test/controllers/recipe_foods_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipeFoodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get recipe_foods_index_url
     assert_response :success
   end
