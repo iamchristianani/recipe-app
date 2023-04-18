37962683e5ce95e428a2d4ac5232eda258fda2c5
Christian Ani <iamchristianani@gmail.com>
2023-04-18 11:28:05 +0100

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
+
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipe_foods, only: [:index]
+
 end
