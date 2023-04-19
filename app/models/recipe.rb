00b39d1f66defa2b32166174ff8c82c273ea2f01
Christian Ani <iamchristianani@gmail.com>
2023-04-19 17:31:26 +0100

    Fix rubocop and Linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
 
-  def total_food_items
-    
-  end
+  def total_food_items; end
 
-  def total_price
-
-  end
+  def total_price; end
 end
