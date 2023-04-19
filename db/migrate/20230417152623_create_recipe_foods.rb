40ad4bffebd2245bf558b577dc630e901dd017a2
developertariq <dev.tariqul.islam@gmail.com>
2023-04-20 00:22:25 +0600

    Refactor migration file

--- db/migrate/20230417152623_create_recipe_foods.rb
+++ db/migrate/20230417152623_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      t.references :recipes, foreign_key: true
-      t.references :foods, foreign_key: true
+      t.references :recipe, foreign_key: true
+      t.references :food, foreign_key: true
 
       t.timestamps
     end
