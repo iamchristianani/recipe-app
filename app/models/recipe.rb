916a37522f224ac684c2e649d6451153f399d5d4
developertariq <dev.tariqul.islam@gmail.com>
2023-04-20 00:22:54 +0600

    Refactor models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :foods
+  has_many :recipe_foods
+
+  def total_food_items
+    recipe_foods.select(:food_id).distinct.count
+  end
+
+  def total_price
+    recipe_foods.includes(:food).sum { |recipe_food| recipe_food.food.price * recipe_food.quantity }
+  end
 end
