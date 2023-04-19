a6a7e9f9ec91bc13c7f70528d4606df68f63c0de
developertariq <dev.tariqul.islam@gmail.com>
2023-04-19 16:31:42 +0600

    Implement food list with crud

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,42 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully saved.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+
+    # authorize! :destroy, @food
+
+    if @food.destroy
+      flash[:success] = 'Food deleted successfully.'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Failed to delete food.'
+      redirect_to foods_path
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
 end
