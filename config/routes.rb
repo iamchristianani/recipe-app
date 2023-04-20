2003ec772ea462ea855402d1010c4f2fdf2b68e8
Christian Ani <iamchristianani@gmail.com>
2023-04-20 15:36:57 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   root "foods#index"
 
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
-  resources :recipe_foods, only: [:index]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
 end
