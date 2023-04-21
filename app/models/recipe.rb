ea5334c2f263e80787ca92ec4562111e416dcf53
developertariq <dev.tariqul.islam@gmail.com>
2023-04-21 20:39:30 +0600

    Implement validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+
+
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 
